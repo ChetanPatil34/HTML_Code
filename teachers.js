40a8df3819c93d202b301405d575bd9e95f28191
ChetanPatil34 <cp9210639@gmail.com>
2024-09-05 23:11:32 +0400

    [ADDED]:CHETAN html,css and javascript project

---
+++ teachers.js
@@ -0,0 +1,7 @@
+function openCard() {
+    document.querySelector('.card-inner').classList.add('flipped');
+}
+
+function closeCard() {
+    document.querySelector('.card-inner').classList.remove('flipped');
+}
